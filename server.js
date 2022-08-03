48709e5a13829912f1d4fffd9b321795acc8910a
Daniel Damasio <damasio.dan7@gmail.com>
2022-08-02 21:17:40 -0300

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/api', require('./routes/api'))
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server listening on port 3000');
 })
