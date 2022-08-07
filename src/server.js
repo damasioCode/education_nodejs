392eab267d5250c560bf14e061e27eeed8254ed2
Daniel Damasio <damasio.dan7@gmail.com>
2022-08-07 00:17:14 -0300

    fix: resolve cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({ origin: '*' }));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
