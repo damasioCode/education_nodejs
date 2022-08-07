c61d93b5e8ad553e8686e26dd22a1212afdcf298
Daniel Damasio <damasio.dan7@gmail.com>
2022-08-07 00:15:11 -0300

    fix: resolve cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import 'dotenv/config'
 import express from 'express'
 import routes from './routes/api.js'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
